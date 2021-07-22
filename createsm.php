d3e2f84925fdebe08014300634820145c97787c0
Ulises Rendón <fdulises@outlook.com>
2021-07-21 22:30:31 -0500

    backup antes de añadir url amigables

--- createsm.php
+++ createsm.php
@@ -9,6 +9,9 @@
     //Ubicación y nombre del sitemap resultante
     define('SITEMAP_OUTPUT', 'sitemap.xml');
 
+    //Ubicación y nombre del sitemap resultante
+    define('SITE_DOMAIN', 'https://neuralpin.com');
+
     //Archivos o carpetas a evitar
     define('IGNORED_LIST', [
         realpath(PATH_DIR.'/404.html'),
@@ -56,7 +59,41 @@
         return $data;
     }
 
+    //Obtenemos listado de paginas a añadir al sitemap
     $list = list_files(PATH_DIR);
 
-    header('Content-type: application/json');
-    echo json_encode($list);
+    //Obtenemos la plantilla xml para el sitemap
+    $xmltemplate = file_get_contents('template.xml');
+
+    //obtenemos el elemento xml para las <ulr>
+	preg_match('/<url>(.*)<\/url>/is',$xmltemplate,$url);
+
+    //Función para Generar <url>
+    function genetareLink( string $link ): string{
+        global $url;
+        $newurl = preg_replace('/\[url\]/i', SITE_DOMAIN.'/'.$link, $url[0]);
+        return $newurl;
+    }
+
+    //Función para generar listado de <url>
+    function parseList( array $data, string $parent = '' ): array{
+        static $links = [];
+        foreach( $data as $k => $i ){
+            if( is_string($i) ) $links[] = genetareLink("{$parent}{$i}");
+            else parseList( $i, "{$k}/" );
+        }
+        return $links;
+    }
+
+    //Generamos listado de elementos <url>
+    $urllist = parseList($list);
+
+    //Remplazamos listado en la plantilla XML
+    $xmltemplate = preg_replace('/<url>(.*)<\/url>/is', implode('',$urllist), $xmltemplate);
+
+    //Generamos sitemap.xml en la ruta especificada
+    $result = file_put_contents(SITEMAP_OUTPUT, $xmltemplate);
+
+    //Mostramos resultado
+    if( $result ) echo 'Sitemap generado correctamente en: ', SITEMAP_OUTPUT;
+    else 'Ocurrió un error al generar Sitemap';
